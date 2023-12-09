e150dc2fe0bf153d1e2d54841dd51729428fc1da
jianjungch <jianjung@gmail.com>
2023-12-10 00:58:43 +0800

    9 commit

--- RFID/obj/Debug/net8.0/RFID.AssemblyInfo.cs
+++ RFID/obj/Debug/net8.0/RFID.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RFID")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c7b95818e00152184bc1c0733d3531ba4d9187")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f05f607966b959b8878bc4fee522b1cf9750d48")]
 [assembly: System.Reflection.AssemblyProductAttribute("RFID")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RFID")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
