13f84159ec5e68c6f820bb0f1b10657ee8d71acb
jianjungch <jianjung@gmail.com>
2023-12-10 13:33:14 +0800

    append action GetMedInfo(ps_id)

--- RFID/ValuesController.cs
+++ RFID/ValuesController.cs
@@ -23,17 +23,17 @@ namespace YourNamespace
         {
             try
             {
-                //using (MySqlConnection connection = new MySqlConnection(connectionString))
-                //{
-                //    connection.Open();
-                //    //  string query = "SELECT COUNT(*) FROM RFID_TEST";
-                //    string query = "SELECT COUNT(*) FROM ccu_rfid_ec.usecase2";
-                //    MySqlCommand cmd = new MySqlCommand(query, connection);
-                //    int count = Convert.ToInt32(cmd.ExecuteScalar());
-                //    return Ok(count);
-                //}
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    //  string query = "SELECT COUNT(*) FROM RFID_TEST";
+                    string query = "SELECT COUNT(*) FROM ccu_rfid_ec.usecase2";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    return Ok(count);
+                }
 
-                  return Ok(888);
+                // return Ok(888);
             }
             catch (Exception ex)
             {
@@ -68,6 +68,53 @@ namespace YourNamespace
                                     loc_id = reader["loc_id"].ToString(),
                                     loc_desc = reader["loc_desc"].ToString(),
                                      // Add other fields here
+                                };
+                                result.Add(data);
+                            }
+                            return Ok(result);
+                        }
+                        else
+                        {
+                            return NotFound();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpGet("GetMedInfo")]
+        public IActionResult GetMedInfo(string ps_id)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();                    
+                    string query = "SELECT * FROM med_info_t WHERE PS_ID = @ps_id ";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@ps_id", ps_id);
+
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            List<dynamic> result = new List<dynamic>();
+                            while (reader.Read())
+                            {
+                                var data = new
+                                {
+                                    ps_id = reader["PS_ID"].ToString(),
+                                    med_item = reader["ITEM"].ToString(),
+                                    med_info = reader["MED_INFO"].ToString(),
+                                    med_date = reader["MED_DATE"].ToString(),
+                                    care_id = reader["CARE_ID"].ToString(),
+                                    // Add other fields here
                                 };
                                 result.Add(data);
                             }
