8fa8dabfbaae03125952d057391693f3ccd16612
jianjungch <jianjung@gmail.com>
2023-12-10 09:06:30 +0800

    14 connect to db4free

--- RFID/ValuesController.cs
+++ RFID/ValuesController.cs
@@ -9,7 +9,7 @@ namespace YourNamespace
     public class RFIDController : ControllerBase
     {
         //private readonly string connectionString = "server=192.168.50.212;uid=root;pwd=test;database=rfiddb";
-       // private readonly string connectionString = "server=db4free.net;uid=rfid_gp_8;pwd=4G*UMZb7ZK.ixxB;database=ccu_rfid_ec";
+        private readonly string connectionString = "server=db4free.net;uid=rfid_gp_8;pwd=4G*UMZb7ZK.ixxB;database=ccu_rfid_ec";
                  
 
         [HttpGet("GetRecordCount")]
@@ -17,17 +17,17 @@ namespace YourNamespace
         {
             try
             {
-                //using (MySqlConnection connection = new MySqlConnection(connectionString))
-                //{
-                //    connection.Open();
-                //    //  string query = "SELECT COUNT(*) FROM RFID_TEST";
-                //    string query = "SELECT COUNT(*) FROM ccu_rfid_ec.usecase2";
-                //    MySqlCommand cmd = new MySqlCommand(query, connection);
-                //    int count = Convert.ToInt32(cmd.ExecuteScalar());
-                //    return Ok(count);
-                //}
-                                
-                return Ok(777);
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+                    //  string query = "SELECT COUNT(*) FROM RFID_TEST";
+                    string query = "SELECT COUNT(*) FROM ccu_rfid_ec.usecase2";
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    return Ok(count);
+                }
+
+                //  return Ok(777);
             }
             catch (Exception ex)
             {
